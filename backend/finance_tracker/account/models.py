53d0455710fdaf44d6a2c32aa6b5b9372dc5a8f1
Saurabh Pawar <saurabhp899@gmail.com>
2024-10-01 20:03:00 +0530

    First commit

---
+++ backend/finance_tracker/account/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.db.models import ForeignKey
+from django.contrib.auth.models import User
+
+
+class Account(models.Model):
+    name = models.CharField(max_length=100)
+    account_type = models.CharField(max_length=100)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = ForeignKey(User, on_delete=models.CASCADE, related_name='accounts')
+
+    def __str__(self):
+        return self.name
